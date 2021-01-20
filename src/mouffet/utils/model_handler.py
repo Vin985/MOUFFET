7be1f0f4f44773518e56a828124a957c7036fa04
Vin985 <sylvain.christin@gmail.com>
2021-01-19 22:02:57 -0400

    Added default datahandler class to model handler Do not create datahandler unless requested Added load_model  classmethod to load a model from options Get_model_instance now static and allows to provide directly a class

--- src/mouffet/utils/model_handler.py
+++ src/mouffet/utils/model_handler.py
@@ -1,16 +1,27 @@
-from abc import ABC, abstractmethod
-from importlib import import_module
-
 import pydoc
+from abc import ABC, abstractmethod
+from pathlib import Path
 
 from ..data.data_handler import DataHandler
+from ..models.dlmodel import DLModel
 from ..options.model_options import ModelOptions
+from ..utils import common as common_utils
 from ..utils import file as file_utils
 
 
 class ModelHandler(ABC):
+
+    DATA_HANDLER_CLASS = DataHandler
+
     def __init__(
-        self, opts=None, opts_path="", dh=None, model=None, model_class=None, **kwargs,
+        self,
+        opts=None,
+        opts_path="",
+        dh=None,
+        dh_class=None,
+        model=None,
+        model_class=None,
+        **kwargs,
     ):
         if not opts:
             if opts_path:
@@ -26,9 +37,10 @@ class ModelHandler(ABC):
         self._data_handler = None
         if model:
             self.model = model
-        if not dh:
-            dh = self.create_data_handler(**kwargs)
-        self.data_handler = dh
+        if dh_class:
+            self.DATA_HANDLER_CLASS = dh_class
+        if dh:
+            self.data_handler = dh
 
         self.scenarios = self.load_scenarios()
 
@@ -44,6 +56,8 @@ class ModelHandler(ABC):
 
     @property
     def data_handler(self):
+        if not self._data_handler:
+            self.data_handler = self.create_data_handler()
         return self._data_handler
 
     @data_handler.setter
@@ -52,26 +66,69 @@ class ModelHandler(ABC):
             dh.opts = self.opts
         self._data_handler = dh
 
-    def create_data_handler(self, dh_class=None, split_funcs=None):
+    def create_data_handler(self):
         data_opts_path = self.opts.get("data_config", "")
         if not data_opts_path:
             raise Exception("A path to the data configuration file must be provided")
         data_opts = file_utils.load_config(data_opts_path)
-        if dh_class and issubclass(dh_class, DataHandler):
-            dh = dh_class(data_opts, split_funcs=split_funcs)
+        if self.DATA_HANDLER_CLASS and issubclass(self.DATA_HANDLER_CLASS, DataHandler):
+            dh = self.DATA_HANDLER_CLASS(data_opts)
         else:
             raise Exception("A subclass of DataHandler must be provided")
         return dh
 
-    def get_model_instance(self, model_opts):
+    @classmethod
+    def load_model(cls, model_opts):
+        """Load a model from the options provided by model_opts. Note: the options
+        saved during training will be loaded and overriden by the relevant options from
+        model_opts (especially the paths). One exception is the "id" and "id_prefixes" options
+        as the one from the old options will always be used to prevent any conflict if the user
+        decides to define a new id for saving the evaluation results.
+
+        Args:
+            model_opts (mouffet.options.ModelOptions): The model options for the current scenario
+
+        Returns:
+            mouffet.model.DLModel: the loaded model
+        """
+        version = model_opts.load_version
+        old_opts = file_utils.load_config(
+            Path(model_opts.model_dir)
+            / model_opts["name"]
+            / str(version)
+            / "network_opts.yaml"
+        )
+        old_opts["data_config"] = model_opts.get("data_config", "")
+        # * To load the model, we use the old opts updated by the scenario, except for the id
+        opts = ModelOptions(old_opts)
+        common_utils.deep_dict_update(
+            opts.opts, model_opts.opts, except_keys=["id", "id_prefixes"]
+        )
+
+        model = cls.get_model_instance(opts)
+        model.load_weights(from_epoch=opts["model"].get("from_epoch", 0))
+        return model
+
+    @staticmethod
+    def get_model_instance(model_opts):
         if not isinstance(model_opts, ModelOptions):
             raise ValueError(
                 "Argument 'model' should be an instance of dlbd.options.ModelOptions or a dict"
             )
+        model_class = model_opts["class"]
+        if issubclass(model_class, DLModel):
+            cls = model_class
+        elif isinstance(model_class, str):
+            cls = pydoc.locate(model_class)
+        else:
+            raise ValueError(
+                "Option 'class' should either be a subclass of",
+                "mouffet.models.dlmodel.DLModel or a string",
+            )
 
         # pkg = import_module(model["package"])
         # cls = getattr(pkg, model["name"])
-        cls = pydoc.locate(model_opts["class"])
+
         return cls(model_opts)
 
     def get_option(self, name, group, default=""):
