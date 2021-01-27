126b540898835e30c302b64500323bcdcc4c2d49
Vin985 <sylvain.christin@gmail.com>
2021-01-27 16:52:52 -0400

    split functions signature

--- src/mouffet/utils/split.py
+++ src/mouffet/utils/split.py
@@ -3,15 +3,14 @@ import random
 from .file import list_folder
 
 
-def random_split(split, paths):
-    audio_path = paths["training_audio_dir"]
-    if not audio_path.exists():
+def random_split(path, split_props, extensions):
+    if not path.exists():
         raise ValueError(
             "'audio_dir' option must be provided to split into training and validation subsets"
         )
-    files = [str(p) for p in audio_path.rglob("*") if p.suffix.lower() == ".wav"]
+    files = [str(p) for p in path.rglob("*") if p.suffix.lower() in extensions]
     n_files = len(files)
-    n_validation = int(split * n_files)
+    n_validation = int(split_props * n_files)
     validation_idx = random.sample(range(0, n_files), n_validation)
     training, validation = [], []
     for i in range(0, n_files):
@@ -29,23 +28,34 @@ def split_list(data, split_props):
     tmp_list = data.copy()
     random.shuffle(tmp_list)
     for proportion in split_props:
-        split_length = int(len(tmp_list) * proportion)
+        split_length = round(len(tmp_list) * proportion)
         splits.append(tmp_list[0:split_length])
         tmp_list = tmp_list[split_length:]
     splits.append(tmp_list)
     return splits
 
 
-def split_folder(path, split_props):
+def split_folder(path, split_props, extensions):
+    """Function if files are ordered with folders. Apply the split to each subfolder with files
+    and continues recursively
+
+    Args:
+        path (Path): path where to start searching for files
+        split_props (list): list of proportions for each split
+        extensions (list): list of file extensions to include
+
+    Returns:
+        list: list with all the splits
+    """
     splits = [[] for i in range(len(split_props) + 1)]
-    dirs, files = list_folder(path, [".wav"])
+    dirs, files = list_folder(path, extensions)
     if files:
         tmp_splits = split_list(files, split_props)
         for i, split in enumerate(tmp_splits):
             splits[i] += split
     if dirs:
         for dir_path in dirs:
-            tmp_splits = split_folder(dir_path, split_props)
+            tmp_splits = split_folder(dir_path, split_props, extensions)
             for i, split in enumerate(tmp_splits):
                 splits[i] += split
     return splits
