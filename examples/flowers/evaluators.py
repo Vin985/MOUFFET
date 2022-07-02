e1beb59f9e1faae521003872e5050b0d6d5b956d
Vin985 <sylvain.christin@gmail.com>
2022-07-02 19:55:51 -0300

    confusion matrix labels

--- examples/flowers/evaluators.py
+++ examples/flowers/evaluators.py
@@ -1,3 +1,4 @@
+from multiprocessing.sharedctypes import Value
 import pandas as pd
 from mouffet.evaluation import Evaluator
 from mouffet import common_utils
@@ -15,18 +16,30 @@ class CustomEvaluator(Evaluator):
         res = []
         for x in columns:
             if x == -1:
-                res.append("Not sure")
+                res.append("Unsure")
             else:
                 res.append(metadata.features["label"].int2str(int(x)))
         return res
 
-    def plot_confusion_matrix(self, data, options):
+    def plot_confusion_matrix(self, data, options, infos):
         cm = confusion_matrix(data["labels"], data["predictions"])
         cm_plot = ConfusionMatrixDisplay(
             confusion_matrix=cm, display_labels=data["label_names"]
         )
         cm_plot.plot()
-        plt.title("test")
+        plt.title(
+            "Confusion matrix for model '{}'\nwith threshold: {}".format(
+                infos["model"], options["threshold"]
+            ),
+            fontweight="bold",
+            fontsize=12,
+        )
+        plt.xlabel("Predicted class", fontweight="bold")
+        plt.ylabel("True class", fontweight="bold")
+        plt.xticks(fontsize=8)
+        plt.yticks(fontsize=8)
+        # cm_plot.ax_.set_xlabel(fontsize=12)
+        # cm_plot.ax_.set_ylabel(fontsize=12)
         return cm_plot
 
     def evaluate(self, data, options, infos):
@@ -34,6 +47,10 @@ class CustomEvaluator(Evaluator):
         preds, labels, meta = data
         # * Get class with the best prediction score
         thresh = options.get("threshold", -1)
+        if thresh > 1:
+            raise ValueError(
+                "The option 'threshold' should only take values between 0 and 1 or -1"
+            )
         npreds = preds.to_numpy()
         top_class = npreds.argmax(axis=1)
         if thresh != -1:
@@ -66,6 +83,7 @@ class CustomEvaluator(Evaluator):
                     "label_names": label_names,
                 },
                 options=options,
+                infos=infos,
             )
 
         return res
